b2986ac6b6183970892450b50dca0a8aa774ef68
Justus Kimutai <justuskiprotich01@gmail.com>
2024-10-11 13:40:26 +0300

    add necessary Changes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
+const newsAdminRoutes = require('./routes/newsRoutes')
 require('dotenv').config();
+const fs = require('fs');
 const cors = require('cors')
 const cookieParser = require ('cookie-parser');
 
@@ -11,14 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
+// Ensure the 'uploads' folder exists
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const corsOptions = {
     origin: function(origin, callback) {
       const allowedOrigins = [
-        'https://www.kaptagatspringswater.co.ke',
-        'https://kaptagatspringswater.co.ke',
         'http://localhost:3000',
+        'http://localhost:5174',
         'http://localhost:5173',
-
       ];
       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
         // Allow requests from the allowed origins or from no origin (e.g., non-browser clients)
@@ -41,6 +47,10 @@ mongoose.connect(process.env.DB_CONNECTION_URI, {
 });
 
 app.use('/users', userRoutes);
+app.use('/adminnews', newsAdminRoutes);
+
+// Serve uploaded files statically
+app.use('/uploads', express.static('uploads'));
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../k-s-frontend/dist')));
---
+++ controllers/newsAdminController.js
@@ -0,0 +1,161 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const path = require('path');
+const User = require('../models/adminNews'); // Adjust the path to your model
+const fs = require('fs');
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname)); // Save file with current timestamp
+    }
+});
+
+const upload = multer({ storage: storage }).single('image');
+
+// User signup
+exports.signup = async (req, res) => {
+    try {
+        const { email, phone, password } = req.body;
+        const existingUser = await User.findOne({ $or: [{ email }, { phone }] });
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email/Phone already exists' });
+        }
+
+        if (!password || !email || !phone) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({
+            password: hashedPassword,
+            email,
+            phone,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully!' });
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+};
+
+// User login
+exports.login = async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        // email = email.toLowerCase();
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Email and password are required' });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_USER_SECRET, { expiresIn: '1h' });
+
+        res.cookie('admins_token', token, {
+            httpOnly: true,
+            secure: false, // Set to true in production
+            maxAge: 3 * 60 * 60 * 1000, // 3 hours
+            sameSite: 'strict',
+        });
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+};
+
+// File upload (photo, title, and body text)
+exports.uploadFile = (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            console.log(err);
+            
+            return res.status(500).json({ message: 'Error uploading file' });
+        }
+
+        const { title, body } = req.body;
+        const image = req.file;
+
+        if (!title || !body || !image) {
+            return res.status(400).json({ message: 'All fields are required (title, body, and image)' });
+        }
+
+        try {
+            const userId = req.userId;  // Extracted from JWT in the middleware
+            
+            // Find the user by ID
+            const user = await User.findById(userId);
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            // Check if user already has an image and delete it
+            if (user.imageUrl) {
+                const oldImagePath = path.join(__dirname, '..', 'uploads', path.basename(user.imageUrl));
+                if (fs.existsSync(oldImagePath)) {
+                    fs.unlinkSync(oldImagePath);  // Delete the old image file
+                }
+            }
+
+            // Create the new image URL
+            const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${image.filename}`;
+
+            // Update the user with new title, body, and image URL
+            user.title = title;
+            user.body = body;
+            user.imageUrl = imageUrl;
+            await user.save();
+
+            // Return a success response with the new image URL
+            res.status(201).json({
+                message: 'File uploaded and previous image deleted successfully!',
+                imageUrl: imageUrl
+            });
+        } catch (error) {
+            console.log(error);
+            
+            res.status(500).json({ message: 'Error saving data to the database' });
+        }
+    });
+};
+
+exports.getNewsData = async (req, res) => {
+    try {
+        const userId = req.userId; // Assuming the userId is passed via middleware (if needed)
+        
+        // Fetch the user’s news data
+        const user = await User.findById(userId).select('title body imageUrl');
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // Return the news data
+        res.status(200).json({
+            title: user.title,
+            body: user.body,
+            imageUrl: user.imageUrl
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching data' });
+    }
+};
+
