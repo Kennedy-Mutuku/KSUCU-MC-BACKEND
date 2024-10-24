31ceebb0d00d175417b889412e8e439b635da429
Justus Kimutai <justuskiprotich01@gmail.com>
2024-10-24 09:23:32 +0300

    temporarilt disable cors

--- app.js
+++ app.js
@@ -19,27 +19,27 @@ if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-const corsOptions = {
-    origin: function(origin, callback) {
-      const allowedOrigins = [
-        'http://localhost:3000',
-        'http://localhost:5174',
-        'http://localhost:5173',
-        'https://ksucu-mc-backend.onrender.com',
-        'https://ksucu-mc-frontend.vercel.app',
-        'ksucu-mc-backend.onrender.com',
-        'ksucu-mc-frontend.vercel.app'
-      ];
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        // Allow requests from the allowed origins or from no origin (e.g., non-browser clients)
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true, // Allow credentials (cookies) to be sent
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: function(origin, callback) {
+//       const allowedOrigins = [
+//         'http://localhost:3000',
+//         'http://localhost:5174',
+//         'http://localhost:5173',
+//         'https://ksucu-mc-backend.onrender.com',
+//         'https://ksucu-mc-frontend.vercel.app',
+//         'ksucu-mc-backend.onrender.com',
+//         'ksucu-mc-frontend.vercel.app'
+//       ];
+//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//         // Allow requests from the allowed origins or from no origin (e.g., non-browser clients)
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     credentials: true, // Allow credentials (cookies) to be sent
+// };
+// app.use(cors(corsOptions));
 
 mongoose.connect(process.env.DB_CONNECTION_URI, {
   useNewUrlParser: true,
@@ -70,5 +70,4 @@ if(process.env.NODE_ENV === 'production'){
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 
