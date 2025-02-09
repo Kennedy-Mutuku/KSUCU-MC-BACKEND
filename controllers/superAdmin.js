2209db9337086c6d1bb6fbb9d469ccd4e325c7f5
Justus Kimutai <justuskiprotich01@gmail.com>
2025-02-09 12:51:39 +0300

    clear commented code

--- controllers/superAdmin.js
+++ controllers/superAdmin.js
@@ -67,16 +67,6 @@ exports.logout = (req, res) => {
     res.status(200).json({ message: 'Logout successful' });
 };
 
-// Get all users for frontend
-// exports.getUsers = async (req, res) => {
-//     try {
-//         const users = await Users.find({}, '-_id name yos'); 
-//         res.status(200).json(users);
-//     } catch (error) {
-//         res.status(500).json({ message: 'Error fetching users', error });
-//     }
-// };
-
 exports.getUsers = async (req, res) => {
     try {
         const users = await Users.find({}, { _id: 0, password: 0, googleId: 0 }); 
